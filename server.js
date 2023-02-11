819b39d295d005da8dd9c9ef6b76ab3547fa8cc3
aftabdotorg <aft4b.akhand@gmail.com>
2023-02-11 15:55:32 +0530

    --minor

--- server.js
+++ server.js
@@ -5,16 +5,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 
-// if (
-//   process.env.NODE_ENV === `production` ||
-//   process.env.NODE_ENV === `staging`
-// ) {
-//   app.use(express.static(`client/build`));
-//   app.get(`*`, (req, res) => {
-//     res.sendFile(path.join(__dirname + `/client/build/index.html`));
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  app.use(express.static('client/build'));
 
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 app.use(express.json());
 app.use(cors());
 
