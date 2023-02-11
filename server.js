cea61439d613093bb16814a1db5754bbd6ec5150
aftabdotorg <aft4b.akhand@gmail.com>
2023-02-11 16:11:53 +0530

    change

--- server.js
+++ server.js
@@ -5,14 +5,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-  //*Set static folder up in production
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-}
 app.use(express.json());
 app.use(cors());
 
